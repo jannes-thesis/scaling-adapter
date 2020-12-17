96444447561cb702a3b6f5d5d1dcddc266871702
jannes <jannes.dev@posteo.net>
2020-12-17 21:20:04 +0100

    Add more threadpool bench workloads

--- adapter-benchmark/src/bin/threadpool/jobs.rs
+++ adapter-benchmark/src/bin/threadpool/jobs.rs
@@ -21,6 +21,14 @@ pub fn read_write_1mb_sync(file_dir: Arc<PathBuf>, index: usize) {
     read_write_Xkb_sync(file_dir, index, 1000);
 }
 
+pub fn read_write_2mb_sync(file_dir: Arc<PathBuf>, index: usize) {
+    read_write_Xkb_sync(file_dir, index, 2000);
+}
+
+pub fn read_write_2mb_nosync(file_dir: Arc<PathBuf>, index: usize) {
+    read_write_Xkb_nosync(file_dir, index, 2000);
+}
+
 pub fn read_write_4mb_sync(file_dir: Arc<PathBuf>, index: usize) {
     read_write_Xkb_sync(file_dir, index, 4000);
 }
@@ -31,6 +39,26 @@ pub fn read_write_buf_sync_1mb(file_dir: Arc<PathBuf>, index: usize) {
 
 pub fn read_write_buf_sync_2mb(file_dir: Arc<PathBuf>, index: usize) {
     read_write_buf_sync_Xkb(file_dir, index, 2000);
+}
+
+pub fn read_write_Xkb_nosync(file_dir: Arc<PathBuf>, index: usize, file_size_kb: u64) {
+    let input_filename = format_input_filename(file_size_kb, index);
+    let input_subdir = format_input_subdir(file_size_kb);
+    let input_filepath = file_dir.join(input_subdir).join(input_filename);
+    let output_filename = format!("wout-{}.txt", index);
+    let input = fs::read_to_string(&input_filepath).unwrap_or_else(|_| {
+        panic!(
+            "error reading file to string, path: {}",
+            input_filepath.to_str().unwrap()
+        )
+    });
+    let output_filepath = file_dir.join(output_filename);
+    let mut output_file = File::create(&output_filepath).expect("unexpected file error");
+    output_file
+        .write_all(input.as_bytes())
+        .expect("error writing input string to output");
+    drop(output_file);
+    fs::remove_file(output_filepath).expect("error removing wout file");
 }
 
 pub fn read_write_Xkb_sync(file_dir: Arc<PathBuf>, index: usize, file_size_kb: u64) {
