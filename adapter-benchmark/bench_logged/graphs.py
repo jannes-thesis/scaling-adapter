f78612f627f72d16107c5205a9bf23789119118d
jannes <jannes.dev@posteo.net>
2020-12-21 17:23:58 +0100

    Analysis scripts for runtime logs threadpool bench

--- adapter-benchmark/bench_logged/graphs.py
+++ adapter-benchmark/bench_logged/graphs.py
@@ -1,7 +1,11 @@
+from typing import Union
 from matplotlib import pyplot
 
 
-def plot_metric_rates(result: list[tuple[int, dict[str, int]]], save_path: str):
+number = Union[float, int]
+
+
+def plot_timeseries_multiple(result: list[tuple[int, dict[str, number]]], save_path: str, title_suffix: str):
     amount_metrics = len(result[0][1].keys())
     fig, axs = pyplot.subplots(
         nrows=amount_metrics, figsize=(20, amount_metrics*10))
@@ -10,8 +14,8 @@ def plot_metric_rates(result: list[tuple[int, dict[str, int]]], save_path: str):
         ys = [tpl[1][metric] for tpl in result]
         axs[i].plot(xs, ys, 'r-')
         axs[i].set_xlabel('runtime in ms')
-        axs[i].set_ylabel(f'{metric} in unit/s')
+        axs[i].set_ylabel(f'{metric} {title_suffix}')
         axs[i].grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.5)
-        axs[i].set_title(metric)
+        axs[i].set_title(f'{metric} {title_suffix} over time')
     fig.savefig(save_path)
     pyplot.close(fig)
