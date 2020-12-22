d39af42946b966e5c9a7df7b513c1ada63c801ee
jannes <jannes.dev@posteo.net>
2020-12-22 11:11:57 +0100

    Traced benchmark results threadpool bench

--- adapter-benchmark/benchmarking/run_multiple_traced.sh
+++ adapter-benchmark/benchmarking/run_multiple_traced.sh
@@ -13,15 +13,21 @@
 # python run_benchmark_traced.py rw_100kb_100us
 # echo "RUNNING 100kb 1ms"
 # python run_benchmark_traced.py rw_100kb_1ms
-echo "RUNNING 4kb 100us with background disk writer"
-python run_benchmark_traced.py rw_4kb_100us-bg_load
-echo "RUNNING 1mb 100ms with background disk writer"
-python run_benchmark_traced.py rw_buf_1mb_100ms-bg_load
+# echo "RUNNING 4kb 100us with background disk writer"
+# python run_benchmark_traced.py rw_4kb_100us-bg_load
+# echo "RUNNING 1mb 100ms with background disk writer"
+# python run_benchmark_traced.py rw_buf_1mb_100ms-bg_load
 # echo "RUNNING 1mb non-buf 1ms"
 # python run_benchmark_traced.py rw_1mb_1ms
 # echo "RUNNING 2mb buf 100ms"
 # python run_benchmark_traced.py rw_buf_2mb_100ms
-echo "RUNNING 4kb 100us with background disk writer 2"
-python run_benchmark_traced.py rw_4kb_100us-bg_load2
-echo "RUNNING 1mb 100ms with background disk writer 2"
-python run_benchmark_traced.py rw_buf_1mb_100ms-bg_load2
+# echo "RUNNING 4kb 100us with background disk writer 2"
+# python run_benchmark_traced.py rw_4kb_100us-bg_load2
+# echo "RUNNING 1mb 100ms with background disk writer 2"
+# python run_benchmark_traced.py rw_buf_1mb_100ms-bg_load2
+echo "RUNNING 2mb read oneshot"
+python run_benchmark_traced.py read_2mb_oneshot
+echo "RUNNING 2mb rw sync oneshot"
+python run_benchmark_traced.py rw_2mb_oneshot
+echo "RUNNING 2mb rw nosync oneshot"
+python run_benchmark_traced.py rw_nosync_2mb_oneshot
