721fe7b913a9e7a977fbd87ef6222fe3d6254e47
jannes <jannes.dev@posteo.net>
2021-01-03 16:33:12 +0100

    More inc-tracer logs and log parsing

--- adapter-benchmark/bench_logged/graphs.py
+++ adapter-benchmark/bench_logged/graphs.py
@@ -1,5 +1,6 @@
 from typing import Union
 from matplotlib import pyplot
+from matplotlib.axes import Axes
 
 
 number = Union[float, int]
@@ -19,3 +20,32 @@ def plot_timeseries_multiple(result: list[tuple[int, dict[str, number]]], save_p
         axs[i].set_title(f'{metric} {title_suffix} over time')
     fig.savefig(save_path)
     pyplot.close(fig)
+
+
+def plot_rwchar_rate(derived: list[tuple[int, dict[str, number]]], 
+                     absolute: list[tuple[int, dict[str, number]]], 
+                     ax: Axes, workload: str, omit_change_points=False):
+    xs = [tpl[0] / 1000 for tpl in derived]
+    ys = [tpl[1]['rwchar/sec'] for tpl in derived]
+    if 'psize' in absolute[0][1]:
+        y2s = [tpl[1]['psize'] for tpl in absolute]
+        ax2 = ax.twinx()
+        ax2.plot(xs, y2s, 'b-')
+        ax2.set_ylabel('pool size')
+        if omit_change_points:
+            last_pool_size = 0
+            xs_filtered = list()
+            ys_filtered = list()
+            for i in range(len(xs)):
+                if y2s[i] != last_pool_size:
+                    last_pool_size = y2s[i]
+                else:
+                    xs_filtered.append(xs[i])
+                    ys_filtered.append(ys[i])
+            xs = xs_filtered
+            ys = ys_filtered
+    ax.plot(xs, ys, 'r-')
+    ax.set_xlabel('runtime in seconds')
+    ax.set_ylabel(f'rwchar/sec')
+    ax.grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.5)
+    ax.set_title(f'rwchar/sec {workload}')
--- adapter-benchmark/bench_logged/main.py
+++ adapter-benchmark/bench_logged/main.py
@@ -1,21 +1,50 @@
 import sys
 import os
 import shutil
+from matplotlib import pyplot
 from parse_log import parse_to_timeseries
 from derived import timeseries_to_rate_all, timeseries_to_derived
-from graphs import plot_timeseries_multiple
+from graphs import plot_timeseries_multiple, plot_rwchar_rate
+
+
+def single_plot(metric, output_name, log_names):
+    if len(log_names) != 2:
+        raise Exception('amount logs not equal to 2')
+    fig, axs = pyplot.subplots(nrows=1, ncols=2, figsize=(20, 10))
+    if 'rwchar-rate' in metric:
+        if metric == 'rwchar-rate':
+            omit = False
+        elif metric == 'rwchar-rate-filtered':
+            omit = True
+        else:
+            raise Exception(f'metric {metric} not supported')
+        for i, log_name in enumerate(log_names):
+            workload = log_name.split('.')[0]
+            parsed = parse_to_timeseries(log_name)
+            derived = timeseries_to_derived(parsed)
+            plot_rwchar_rate(derived, parsed, axs[i], workload, omit)
+        fig.savefig(output_name)
+        pyplot.close(fig)
+    else: 
+        raise Exception(f'metric {metric} not supported')
 
 
 if __name__ == '__main__':
-    log_names = sys.argv[1:]
-    for log_name in log_names:
-        workload = log_name.split('.')[0]
-        if os.path.exists(workload):
-            shutil.rmtree(workload)
-        os.mkdir(workload)
-        parsed = parse_to_timeseries(log_name)
-        original_rates = timeseries_to_rate_all(parsed)
-        derived = timeseries_to_derived(parsed)
-        plot_timeseries_multiple(parsed, f'{workload}/original-absolute.png', 'total since start')
-        plot_timeseries_multiple(original_rates, f'{workload}/original-rates.png', 'in units/sec')
-        plot_timeseries_multiple(derived, f'{workload}/derived.png', '')
+    if sys.argv[1] == '--single':
+        metric = sys.argv[2]
+        output_name = sys.argv[3]
+        log_names = sys.argv[4:]
+        single_plot(metric, output_name, log_names)
+    else:
+        log_names = sys.argv[1:]
+        for log_name in log_names:
+            workload = log_name.split('.')[0]
+            if os.path.exists(workload):
+                shutil.rmtree(workload)
+            os.mkdir(workload)
+            parsed = parse_to_timeseries(log_name)
+            original_rates = timeseries_to_rate_all(parsed)
+            derived = timeseries_to_derived(parsed)
+            plot_timeseries_multiple(parsed, f'{workload}/original-absolute.png', 'total since start')
+            plot_timeseries_multiple(original_rates, f'{workload}/original-rates.png', 'in units/sec')
+            plot_timeseries_multiple(derived, f'{workload}/derived.png', '')
