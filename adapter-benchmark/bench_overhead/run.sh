00429f087e870da7ae27cacef4edf5abcd465d54
jannes <jannes.dev@posteo.net>
2021-01-16 19:56:15 +0100

    Overhead results

--- adapter-benchmark/bench_overhead/run.sh
+++ adapter-benchmark/bench_overhead/run.sh
@@ -3,20 +3,20 @@ load_type=$1
 worker_function=$2
 num_jobs=$3
 out_prefix=$4
+pool_size=$5
 files_dir="/ssd2/adapter-benchmark/files"
-pool_size=16
 
 params_str="$load_type $worker_function $num_jobs $files_dir"
 cmd_no_adapter="../../target/release/threadpool single fixed $pool_size $params_str"
 cmd_with_adapter="../../target/release/threadpool single fixed-overhead $pool_size $params_str"
 
-hyperfine --min-runs 5 \
+hyperfine --min-runs 10 \
     --style basic \
     --prepare 'sudo /bin/clear_page_cache' \
     "${cmd_no_adapter}" \
     --export-json "${out_prefix}-fixed.json"
 
-hyperfine --min-runs 5 \
+hyperfine --min-runs 10 \
     --style basic \
     --prepare 'sudo /bin/clear_page_cache' \
     "${cmd_with_adapter}" \
--- adapter-benchmark/bench_overhead/run_multiple.sh
+++ adapter-benchmark/bench_overhead/run_multiple.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# previous runs were all with pool size 16
 # ./run.sh oneshot read_write_4kb_sync 1000 os-rw4kb-1k
-./run.sh oneshot read_write_4kb_sync 100000 os-rw4kb-100k
-./run.sh every100us read_write_4kb_sync 100000 100us-rw4kb-10k
-./run.sh oneshot read_write_buf_sync_1mb 1000 os-rwbuf1mb-1k
-./run.sh every100ms read_write_buf_sync_1mb 1000 100ms-rwbuf1mb-1k
+# ./run.sh oneshot read_write_4kb_sync 100000 os-rw4kb-100k
+# ./run.sh every100us read_write_4kb_sync 100000 100us-rw4kb-10k
+# ./run.sh oneshot read_write_buf_sync_1mb 1000 os-rwbuf1mb-1k
+# ./run.sh every100ms read_write_buf_sync_1mb 1000 100ms-rwbuf1mb-1k
+# ./run.sh oneshot read_write_buf_sync_2mb 2000 os-rwbuf2mb-2k 32
+# ./run.sh oneshot read_2mb 30000 os-read2mb-30k 16  
+./run.sh oneshot read_write_2mb_sync 10000 os-rw2mb-10k 32
+./run.sh oneshot read_write_2mb_nosync 20000 os-rwnosync2mb-20k 32
