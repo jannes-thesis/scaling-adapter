4e067f0c91cec2a5c2f905d0ec062c87f1c70269
jannes <jannes.dev@posteo.net>
2021-01-30 12:17:27 +0100

    Results traced rwnosync2mb

--- adapter-benchmark/bench_traced/combine_results.sh
+++ adapter-benchmark/bench_traced/combine_results.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 n=$1
-result_dirs=($(ls -l | grep "run-read" | tail -n $n | awk '{print $9}'))
+result_dirs=($(ls -l | grep "run-rw" | tail -n $n | awk '{print $9}'))
 repeated_dir="${result_dirs[0]}-repeated"
 mkdir $repeated_dir
 for ((i = 0 ; i < $n; i++)); do
